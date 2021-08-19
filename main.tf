47310c2bb5c3641149a2f4350d909daf5d056811
EC2 Default User <ec2-user@ip-172-31-91-146.ec2.internal>
2021-08-19 04:41:19 +0000

    teejay demo create cluster

---
+++ main.tf
@@ -0,0 +1,138 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+locals {
+  name = "teejay-demo"
+
+  tags = {
+    Project   = "Teejay Terraform K8s Solution"
+    Terraform = "True"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_iam_role" "cluster" {
+  name_prefix = "eks-cluster-${local.name}-"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+
+  tags = local.tags
+}
+
+# Attach IAM policies:
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-iam-awsmanpol.html
+resource "aws_iam_role_policy_attachment" "cluster_eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+# Enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "cluster_eks_vpc_resource_controller" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_eks_cluster" "cluster" {
+  name     = local.name
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.20"
+
+  vpc_config {
+    subnet_ids = data.aws_subnet_ids.default.ids
+  }
+
+  # Make sure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_eks_cluster_policy,
+    aws_iam_role_policy_attachment.cluster_eks_vpc_resource_controller,
+  ]
+
+  tags = local.tags
+}
+
+# Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+
+resource "aws_iam_role" "nodes" {
+  name_prefix = "eks-nodes-${local.name}-"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_ec2_container_registry_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "default"
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids      = data.aws_subnet_ids.default.ids
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
+  }
+
+  #Set Instance Type
+  instance_types = ["t3.medium"]
+
+  # Make sure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.nodes_eks_cni_policy,
+    aws_iam_role_policy_attachment.nodes_ec2_container_registry_read_only,
+  ]
+
+  tags = local.tags
+}
+
+#PrintOut the K8S API endpoint
+output "endpoint" {
+  value = aws_eks_cluster.cluster.endpoint
+}
+
+#PrintOut the K8S KUBECONFIG
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.cluster.certificate_authority[0].data
+}
